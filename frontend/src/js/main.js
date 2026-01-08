01a76878c4dc1502f995944bbdf16184f3c5c365
Bernardo de Castro <bernardocmfgomes@aluno.ufsj.edu.br>
2026-01-07 22:45:05 -0300

    update: Design e informações do site atualizadas com sucesso

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,3 +1,15 @@
+const modal = document.getElementById('modal');
+const openBtn = document.getElementById('openModal');
+const closeBtn = document.getElementById('closeModal');
+
+openBtn.addEventListener('click', () => {
+  modal.classList.remove('hidden');
+});
+
+closeBtn.addEventListener('click', () => {
+  modal.classList.add('hidden');
+});
+
 document.querySelectorAll("[data-animate]").forEach(el => {
   el.classList.add("fade-in")
 })
